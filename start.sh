27edc4aaacbe505ecb29bc51bcd029af151a0a83
maxxxr <71984415+maxxxr@users.noreply.github.com>
2020-09-28 09:02:46 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
  echo $RESOURCE_GROUP
  #RESOURCE_LOCATION=`az group list --query '[0].location' --output tsv`
  #echo  $RESOURCE_LOCATION
-
+$ADMIN_PASSWORD= 'asferw;jsdfiweke1_'
 
 # -----------------
 # Create DC1 VM
@@ -13,4 +13,19 @@ az vm create \
     --name DC1 \
     --image win2016datacenter \
     --admin-username adminlogin \
-    --admin-password 'asferw;jsdfiweke1_'
+    --admin-password $ADMIN_PASSWORD \
+    --public-ip-address ""
+    
+# -----------------
+# Create CL1 VM
+# -----------------
+
+az vm create \
+    --resource-group $RESOURCE_GROUP \
+    --name CL1 \
+    --image win2016datacenter \
+    --admin-username adminlogin \
+    --admin-password $ADMIN_PASSWORD \
+    
+az network public-ip show --name CL1PublicIP --resource-group learn-0dd5fc86-dcc6-4558-a057-6332fceaf6ce --query ipAddress -o tsv
+
