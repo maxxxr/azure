40d17a5d84b3420fd7b7903f57161337479d9c62
maxxxr <71984415+maxxxr@users.noreply.github.com>
2020-09-28 08:21:11 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
  RESOURCE_GROUP=$(az group list --query [].name -o tsv)
  echo $RESOURCE_GROUP
- RESOURCE_LOCATION=`az group list --query '[0].location' --output tsv`
- echo  $RESOURCE_LOCATION
+ #RESOURCE_LOCATION=`az group list --query '[0].location' --output tsv`
+ #echo  $RESOURCE_LOCATION
+
+
+# -----------------
+# Create DC1 VM
+# -----------------
+
+az vm create \
+    --resource-group $RESOURCE_GROUP \
+    --name myVM \
+    --image win2016datacenter \
+    --admin-username admin
+    -admin-password asferw;jsdfiweke1_
