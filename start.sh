77deda66cf920d59d317d2fcba4c1f0b005fca5a
maxxxr <71984415+maxxxr@users.noreply.github.com>
2020-09-28 08:09:03 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
  RESOURCE_GROUP=$(az group list --query [].name -o tsv)
  echo $RESOURCE_GROUP
  RESOURCE_LOCATION=`az group list --query '[0].location' --output tsv`
- echo  RESOURCE_LOCATION
+ echo  $RESOURCE_LOCATION
